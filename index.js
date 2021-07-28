6797eb70edfeb900a18abb1d42dfd0c89ce5caf9
Nonny177 <55391624+Nonny177@users.noreply.github.com>
2021-07-28 22:49:14 +0200

    Changed content of navbar

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.body.style.backgroundColor = "#4682B4";
+document.body.style.backgroundColor = "#FFF0F5";
 document.body.style.color = 'white';
 
 
@@ -17,24 +17,40 @@ const createNav = () => {
     unOrdered.style.alignContent = "center";
     unOrdered.style.listStyleType = "none";
     
-    const homeList = document.createElement('li');
+    const homeList = document.createElement('a');
+    homeList.href = "index.html"; 
+    homeList.style.textDecoration = "none";
+    aboutList.style.color = 'white';
     homeList.style.flex = "auto"
     homeList.style.textAlign = "center";
 
-    const aboutList = document.createElement('li');
+    const aboutList = document.createElement('a');
+    aboutList.href = "aboutN.html"; 
+    aboutList.style.textDecoration = "none";
+    aboutList.style.color = 'white';
     aboutList.style.flex = "auto"
     aboutList.style.textAlign = "center";
 
+    const about2List = document.createElement('a');
+    about2List.href = "aboutT.html"; 
+    about2List.style.textDecoration = "none";
+    about2List.style.color = 'white';
+    about2List.style.flex = "auto"
+    about2List.style.textAlign = "center";
+
     homeList.setAttribute('class', 'tab');
     aboutList.setAttribute('class', 'tab');
+    about2List.setAttribute('class', 'tab');
 
     homeList.textContent = 'Home';
     aboutList.textContent = 'About Us';
+    about2List.textContent = 'About Thami';
 
     nav.classList = 'navbar';
 
     unOrdered.appendChild(homeList);
     unOrdered.appendChild(aboutList);
+    unOrdered.appendChild(about2List);
     nav.appendChild(unOrdered);
 
     return nav;
