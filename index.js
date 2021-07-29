5bd082e46bce5d2018a626121feaebeb7d794a94
Nonny177 <55391624+Nonny177@users.noreply.github.com>
2021-07-29 20:10:35 +0200

    Added a background image

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 document.body.style.backgroundColor = "#FFF0F5";
 document.body.style.color = 'white';
 
-
-const div = document.createElement('div');    
-div.style.color = 'blue'; 
-div.textContent = 'Hello World!'
-div.innerHTML = '<span>Hello World!</span>'; 
-
-
-
 const createNav = () => {
     const nav = document.createElement('nav');
-    nav.style.cssText = 'border: 1px solid #ccc; background: pink';
+    nav.style.cssText = 'border: 1px solid #ccc; background: pink; height: 55px';
+    
     const unOrdered = document.createElement('ul');
     unOrdered.style.display = "flex";
     unOrdered.style.alignContent = "center";
     unOrdered.style.listStyleType = "none";
+
+    let img = document.createElement('img');
+    img.src = 'girlcode.png';
+    img.style.cssText = 'width: 90px; height: 50px';
+    img.style.marginTop = '-15px';
     
     const homeList = document.createElement('a');
     homeList.href = "index.html"; 
     homeList.style.textDecoration = "none";
-    aboutList.style.color = 'white';
-    homeList.style.flex = "auto"
+    homeList.style.color = 'white';
+    homeList.style.flex = "auto";
     homeList.style.textAlign = "center";
 
     const aboutList = document.createElement('a');
     aboutList.href = "aboutN.html"; 
     aboutList.style.textDecoration = "none";
     aboutList.style.color = 'white';
-    aboutList.style.flex = "auto"
+    aboutList.style.flex = "auto";
     aboutList.style.textAlign = "center";
 
     const about2List = document.createElement('a');
     about2List.href = "aboutT.html"; 
     about2List.style.textDecoration = "none";
     about2List.style.color = 'white';
-    about2List.style.flex = "auto"
+    about2List.style.flex = "auto";
     about2List.style.textAlign = "center";
 
     homeList.setAttribute('class', 'tab');
@@ -48,6 +46,7 @@ const createNav = () => {
 
     nav.classList = 'navbar';
 
+    unOrdered.appendChild(img);
     unOrdered.appendChild(homeList);
     unOrdered.appendChild(aboutList);
     unOrdered.appendChild(about2List);
@@ -56,7 +55,21 @@ const createNav = () => {
     return nav;
   };
 
+  const image2 = document.createElement('img');
+  image2.src = 'flower.jpg';
+  image2.style.cssText = 'width: 100%; height: 600px';
+  image2.style.filter = "blur(4px)";
+
+  const text = document.createElement('h1');
+  text.classList.add("bg-text");
+  text.innerHTML= "HOORAY!!!"+'<br>'+"Welcome to the world of " + '<br />' +"GIRLCODERS." + '<br>' + "Find all there is to know about our beautiful girls on their about pages.";
+
+  
 const body = document.querySelector('body');
 const navbar = createNav();
 const mainDiv = document.getElementById('content');
 body.appendChild(navbar);
+body.appendChild(image2);
+body.appendChild(text);
+
+
